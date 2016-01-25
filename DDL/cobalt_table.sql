930dcef74ece05bbbf2cf886d73b7b17381d29fb
setrar <setrar@valiha.com>
2016-01-25 18:20:59 -0500

    Create cobalt_table.sql

---
+++ DDL/cobalt_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE cobalt (
+   price_date DATE NOT NULL,
+   open DECIMAL(10, 6 ) NULL,
+   high DECIMAL(10, 6 ) NULL,
+   low DECIMAL(10, 6 ) NULL,
+   close DECIMAL(10, 6 ) NULL,
+   volume DECIMAL(10, 6 ) NULL,
+   adjusted DECIMAL(10, 6 ) NULL
+);
