4f0278e80be2d7814dd13c75de383d3307718040
setrar <setrar@valiha.com>
2016-02-01 17:17:34 -0500

    Rearranging

---
+++ 2.COBALT/create_personne.sql
@@ -0,0 +1,5 @@
+CREATE TABLE personne (
+ nom VARCHAR (100),
+ adresse VARCHAR (300),
+ departement VARCHAR (400)
+);
---
+++ 2.COBALT/create_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE loans (
+ loan_date DATE NOT NULL,
+ amount DECIMAL(10,6) NULL
+);
