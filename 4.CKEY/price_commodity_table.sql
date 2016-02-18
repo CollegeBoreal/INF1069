b66754dbfdb9e91e5a3c4c6f0ca92535e811a811
FassDeKU <jonathandesrosiers@hotmail.com>
2016-02-18 15:17:49 -0500

    nouveau ajout

--- 4.CKEY/price_commodity_table.sql
+++ 4.CKEY/price_commodity_table.sql
@@ -1,9 +1,11 @@
 CREATE TABLE price (
+   id INT NOT NULL, 
    price_date DATE NOT NULL,
    open DECIMAL(10, 6 ) NULL,
    high DECIMAL(10, 6 ) NULL,
    low DECIMAL(10, 6 ) NULL,
    close DECIMAL(10, 6 ) NULL,
    volume DECIMAL(10, 6 ) NULL,
-   adjusted DECIMAL(10, 6 ) NULL
-);
+   adjusted DECIMAL(10, 6 ) NULL,
+   PRIMARY KEY (id, price_date)
+);I
