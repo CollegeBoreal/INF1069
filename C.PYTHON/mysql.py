76a693c948ff05e7932336b90425096873a1ab64
setrar <setrar@valiha.com>
2016-04-11 17:17:04 -0400

    Update mysql.py

--- C.PYTHON/mysql.py
+++ C.PYTHON/mysql.py
@@ -1,6 +1,16 @@
-from mysql.connector import (connection)
+#!/usr/bin/python
+import MySQLdb
 
-cnx = connection.MySQLConnection(user='artists', password='artists_1',
-                                 host='192.168.99.1',
-                                 database='artists')
-cnx.close()
+db = MySQLdb.connect( user='etudiants', 
+                      passwd='etudiants_1',
+                      host='192.168.99.100',
+                      db='sakila')
+
+cur = db.cursor()
+
+cur.execute("SELECT * FROM country")
+
+for row in cur.fetchall():
+    print row[0]
+
+db.close()
